a1f41fef9b972568b2d0f2639ccfd562f4d71b8d
Dmitry Pudov <dimapudov99@gmail.com>
2019-06-04 19:44:36 +0300

    Working version

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -11,8 +11,10 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setTitle("Пудов. Лабораторная работа №8. Компьютерная графика");
+        primaryStage.setScene(new Scene(root, 1000, 600));
+        primaryStage.setMaximized(true);
+        primaryStage.setResizable(false);
         primaryStage.show();
     }
 
